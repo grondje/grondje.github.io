55eb0ec1838015a38d70c6f1ebe455cbd9f90463
nikita_k <nikitak@kodeks.ru>
2015-09-22 19:21:05 +0300

    probably v.0.0.1 with all tasks done added links attach added new validation for searching

--- dist/js/search.js
+++ dist/js/search.js
@@ -1,34 +1,60 @@
 $(function() {
-	var template = "<div class='mdl-card mdl-cell mdl-cell--4-col-desktop mdl-cell--4-col-tablet mdl-cell--12-col-phone mdl-shadow--4dp'><div class='mdl-card__title mdl-color--indigo-600 mdl-color-text--white'><h2 class='mdl-card__title-text'>card-title</h2></div><div class='mdl-card__supporting-text'>card-text</div><div class='mdl-card__actions'> <a href='card-link' class='mdl-button mdl-js-button mdl-button--primary mdl-js-ripple-effect' data-upgraded=',MaterialButton,MaterialRipple'>Перейти по ссылке<span class='mdl-button__ripple-container'><span class='mdl-ripple'></span></span></a></div></div>"
+	var template = "<div class='mdl-card mdl-cell mdl-cell--4-col-desktop mdl-cell--4-col-tablet mdl-cell--12-col-phone mdl-shadow--4dp'><div class='mdl-card__title mdl-color--indigo-600 mdl-color-text--white'><h2 class='mdl-card__title-text'>card-title</h2></div><div class='mdl-card__supporting-text'>card-text</div><div class='mdl-card__actions'> <a target='_blank' href='card-link' class='mdl-button mdl-js-button mdl-button--primary mdl-js-ripple-effect' data-upgraded=',MaterialButton,MaterialRipple'>Перейти по ссылке<span class='mdl-button__ripple-container'><span class='mdl-ripple'></span></span></a></div></div>",
+		searchValue,
+		count = 0,
+		newPages = [],
+		newPage = {}
+
 
 	$('#form').on('submit',(function(e) {
 		e.preventDefault();
-		var searchValue = $('#wiki-inp_s').val();
-		console.log(searchValue);
-
-		$.ajax({
-			url: '//en.wikipedia.org/w/api.php',
-  			data: { 
-  				action: 'query',
-  				list: 'search', 
-  				srsearch: searchValue, 
-  				format: 'json' , 
-  				prop: 'info', 
-  				inprop: 'url', 
-  				generator: 'search', 
-  				gsrsearch: searchValue,
-  				srlimit: 10
-  			},
- 			 dataType: 'jsonp',
-  			success: function (x) {
-	  				console.log(x);
-	  				x.query.search.forEach(function(item, i, search){
-	  					var newTemplate = template.replace('card-title', x.query.search[i].title);
-	  					newTemplate = newTemplate.replace('card-text', x.query.search[i].snippet)
-	  					$('.search_results .mdl-grid').append(newTemplate);
-	  				});
-  				}
-		});
+		$('.search_results .mdl-grid').html('');
+		$('.mdl-textfield').removeClass('is-invalid');
+		searchValue = $('#wiki-inp_s').val();
+		if (!searchValue) {
+			$('.mdl-textfield').addClass('is-invalid');
+		}
+		else{
+			$('.mdl-spinner').addClass('is-active');
+			$.ajax({
+				url: '//en.wikipedia.org/w/api.php',
+				data: { 
+					action: 'query',
+					list: 'search',
+					srsearch: searchValue, 
+					format: 'json' , 
+					prop: 'info', 
+					inprop: 'url', 
+					generator: 'search', 
+					gsrsearch: searchValue
+				},
+	 			dataType: 'jsonp',
+				success: function (x) {
+						$('.mdl-spinner').removeClass('is-active');
+						$.each(x.query.pages, function(index, val) {
+							var pageTitle = val.title;
+							var pageUrl = val.canonicalurl;
+							newPage = {
+								pageUrl,
+								pageTitle
+							};
+							newPages.push(newPage);
+							count++;
+						});
+						x.query.search.forEach(function(item, i, search){
+							for (var j = 0; j < newPages.length; j++) {
+								if(x.query.search[i].title === newPages[j].pageTitle){
+									x.query.search[i].url = newPages[j].pageUrl;
+								}
+							}
+							var newTemplate = template.replace('card-title', x.query.search[i].title);
+							newTemplate = newTemplate.replace('card-text', x.query.search[i].snippet);
+							newTemplate = newTemplate.replace('card-link', x.query.search[i].url)
+							$('.search_results .mdl-grid').append(newTemplate);
+						});
+					}
+			});
+		}
 	}));
 
 });
